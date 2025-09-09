4f6d4882639e3d50934a4560c5eb9fa42d0a9ad3
rich7420 <rc910420@gmail.com>
2025-09-09 14:07:19 +0800

    delete

--- RCAEval/gnn_kan_module/kan_components/feature_extraction.py
+++
@@ -1,8 +0,0 @@
-"""
-This module is deprecated.
-All feature extraction logic has been moved to the `RCAEval.gnn_kan_module.processors` sub-package.
-This file is kept for backward compatibility during transition and will be removed in a future version.
-"""
-
-# To prevent accidental usage, you can raise an error on import.
-# raise DeprecationWarning("This module is deprecated. Use processors sub-package instead.")
